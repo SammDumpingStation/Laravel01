b5a816effba45c78bdd6b6ee93fa5772fbbd7cc1
Samm Caagbay <sammcaag@gmail.com>
2024-05-12 14:34:01 +0800

    👍 Setting CSS and Fonts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/general.css">
+  
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&display=swap" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body>
+  <form action="" method="post">
+    <h1>LARAVEL V-01</h1>
+    <section id="login-body">
+      <div class="input-field">
+        <label for="">Username</label>
+        <input type="text" name="username" id="">
+      </div>
+      <div class="input-field">
+        <label for="">Password</label>
+        <input type="password" name="password" id="">
+      </div>
+      <button id="login-button">LOGIN</button>
+      <p>Don't have an account? <a href="">Sign-up</a> </p>
+    </section>
+  </form>
+</body>
+</html>
