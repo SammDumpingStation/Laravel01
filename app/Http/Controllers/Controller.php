50ba623893d9a602c2a733a2d7783e53a22fedd5
Samm Caagbay <sammcaag@gmail.com>
2024-05-13 18:49:22 +0800

    👍 Practicing Laravel

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
 abstract class Controller
 {
-    //
+    public function register(Request $request) {
+        $validate = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+        return "You have registered Yourself";
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/css/general.css">
+  <link rel="stylesheet" href="/css/main.css">
   
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -11,20 +12,27 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="" method="post">
-    <h1>LARAVEL V-01</h1>
-    <section id="login-body">
-      <div class="input-field">
-        <label for="">Username</label>
-        <input type="text" name="username" id="">
-      </div>
-      <div class="input-field">
-        <label for="">Password</label>
-        <input type="password" name="password" id="">
-      </div>
-      <button id="login-button">LOGIN</button>
-      <p>Don't have an account? <a href="">Sign-up</a> </p>
-    </section>
-  </form>
+  <main>
+    <div id="black-circle"></div>
+
+    <form action="/register" method="post">
+      @csrf
+      <h1>LARAVEL V-01</h1>
+
+      <section id="login-body">
+        <div class="input-field">
+          <label for="">Username</label>
+          <input type="text" name="username" class="input">
+        </div>
+        <div class="input-field">
+          <label for="">Password</label>
+          <input type="password" name="password" class="input">
+        </div>
+        <input type="submit" value="LOGIN" id="login-button">
+        <p>Don't have an account? <a href="" id="link">Sign-up</a> </p>
+      </section>
+    </form>    
+  </main>
+
 </body>
 </html>
